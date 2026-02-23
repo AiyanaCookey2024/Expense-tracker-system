e87ca877f276850c71d24506959e8b0591a6fcb0
AiyanaCookey <163604235+AiyanaCookey@users.noreply.github.com>
2026-02-23 10:07:19 +0000

    after adding new models and fixing api errors - it is now working as expected

--- moz-todo-react/src/App.jsx
+++ moz-todo-react/src/App.jsx
@@ -1,14 +1,52 @@
+import { useState } from "react"; 
 import "./App.css";
 
+const Greeting = ({ phrase, name }) => (
+  <h1>{phrase} {name}!</h1>
+)
 
-function App(props) {   
+
+const Box = ({name, onNameChange, phrase, onPhraseChange}) => {     
   return (
-    <>
-      <h1>{props.greeting}, {props.name}!</h1> {/* modified this line */}
-      <button type="button">Click me!</button>
+   <div className="box">
+      <Greeting name={name} phrase={phrase} />             
+      <input value={phrase} onChange={ onPhraseChange } />      
+      <input value={name} onChange={ onNameChange } />      
+    </div>
+  );
+};
+
+
+const App = () => {  
+  const [name, setName] = useState('Guest');  
+  const [phrase, setPhrase] = useState('Hi'); 
+
+
+  const handleNameChange = (event) => { 
+    const value = event.target.value;    
+    const formatted = value.charAt(0).toUpperCase() + value.slice(1);
+    setName(formatted); 
+  }
+
+
+  const handlePhraseChange = (event) => { 
+    const value = event.target.value;    
+    const formatted = value.charAt(0).toUpperCase() + value.slice(1);
+    setPhrase(formatted); 
+  }
+
+
+  return (
+    <>      
+      <Box 
+        name={name} 
+        onNameChange={handleNameChange}  
+        phrase={phrase}    
+        onPhraseChange={handlePhraseChange}
+      /> 
     </>
   );
-}
+};
 
 
 export default App;
