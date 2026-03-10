e821fc5c3e1d5f10e777dc4b465e33c47904eba0
AiyanaCookey <163604235+AiyanaCookey@users.noreply.github.com>
2026-03-10 17:26:42 +0000

    allowing hosts to view app in render (deployment stage)

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://expense-tracker-system-1-dhpr.onrender.com]", "https://expense-tracker-system-1l5v.onrender.com", "localhost"]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
