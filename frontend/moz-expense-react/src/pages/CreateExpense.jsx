3bd3a42bc99ee850aa4eeb44419addd9dde01a5d
AiyanaCookey <163604235+AiyanaCookey@users.noreply.github.com>
2026-03-11 17:18:36 +0000

    .

--- frontend/moz-expense-react/src/pages/CreateExpense.jsx
+++ frontend/moz-expense-react/src/pages/CreateExpense.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 
 function CreateExpense() {
 
-    const apiURL = import.meta.env.VITE_DJANGO_API_URL="https://expense-tracker-system-1l5v.onrender.com"
+    const apiURL = import.meta.env.VITE_DJANGO_API_URL || "http://127.0.0.1:8000/api";
 
     const navigate = useNavigate();
 
