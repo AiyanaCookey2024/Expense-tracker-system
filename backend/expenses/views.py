eb273379bdb4539396aa96a71cc4262429227776
AiyanaCookey <163604235+AiyanaCookey@users.noreply.github.com>
2026-03-17 20:40:27 +0000

    redploying backend

--- backend/expenses/views.py
+++ backend/expenses/views.py
@@ -8,11 +8,14 @@ from .models import Expense, SalaryPeriod, Budget
 class BudgetView(viewsets.ModelViewSet):
     serializer_class = BudgetSerializer
     queryset = Budget.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
 
 class SalaryPeriodView(viewsets.ModelViewSet):
     serializer_class = SalaryPeriodSerializer
     queryset = SalaryPeriod.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
 
 class ExpenseView(viewsets.ModelViewSet):
     serializer_class = ExpenseSerializer
     queryset = Expense.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
